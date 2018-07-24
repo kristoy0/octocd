72ebd3d1e0a85fff43a61869a16fc260d8fb314b
kristoy0 <kristoy0@hotmail.com>
2018-07-24 04:28:40 +0300

    Added tox for easier testing

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='octocd',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/kristoy0/octocd',
+    license='MIT',
+    author='kristo',
+    author_email='kristoy0@hotmail.com',
+    description='A simple continuous delivery service',
+    zip_safe=False,
+
+)
